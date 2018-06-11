2d5a33a661a479a76b34f4a06166977ab02e80ce
Stepan Mracek <stepan.mracek@gmail.com>
2018-06-11 22:06:58 +0200

    example using ngx-mqtt@5.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MqttService } from 'ngx-mqtt';
+
+import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+
+interface IMqttSubscription {
+  subscription?: Subscription;
+  id: number;
+  payload: any;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  private counter = 0;
+
+  mqttSubscriptions: IMqttSubscription[] = [];
+
+  constructor(private mqtt: MqttService) { }
+
+  ngOnInit() {
+    this.observe();
+    for (let i = 0; i < 10; i++) {
+      setTimeout(() => this.observe(), i*100);
+    }
+  }
+
+  observe() {
+    let s: IMqttSubscription = {
+      id: this.counter++,
+      payload: null
+    };
+    s.subscription = this.mqtt
+      .observe("topic")
+      .pipe(
+        map(v => { console.log(v, s.id); return v }),
+        map(v => v.payload),
+      )
+      .subscribe(msg => { s.payload = msg; });
+    this.mqttSubscriptions.push(s);
+  }
+
+  unsubscribe(s: IMqttSubscription) {
+    s.subscription.unsubscribe();
+    this.mqttSubscriptions.splice(this.mqttSubscriptions.indexOf(s), 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MqttModule, MqttService } from 'ngx-mqtt';
 
 import { AppComponent } from './app.component';
 
+export function mqttServiceFactory() {
+  return new MqttService({
+    connectOnCreate: true,
+    hostname: 'localhost',
+    port: 1884
+  });
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MqttModule.forRoot({
+      provide: MqttService,
+      useFactory: mqttServiceFactory
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
